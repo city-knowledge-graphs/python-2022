dd4d0a861b1a23c3a50c79d64183b8587de51743
ernestojimenezruiz <ernesto.jimenez.ruiz@gmail.com>
2022-02-07 13:57:40 +0000

    Lab 2 examples

---
+++ lab2/loadOntology.py
@@ -0,0 +1,35 @@
+'''
+Created on 19 Jan 2021
+
+@author: ejimenez-ruiz
+'''
+from owlready2 import *
+
+
+def getClasses(onto):        
+    return onto.classes()
+    
+def getDataProperties(self):        
+    return onto.data_properties()
+    
+def getObjectProperties(self):        
+    return onto.object_properties()
+    
+def getIndividuals(self):        
+    return onto.individuals()
+
+
+
+def loadOntology(urionto):
+    
+    #Method from owlready
+    onto = get_ontology(urionto).load()
+    
+    print("Classes in Ontology: " + str(len(list(getClasses(onto)))))
+    for cls in getClasses(onto):                
+        print("\t"+cls.iri)
+
+
+#Load ontology
+urionto="http://protege.stanford.edu/ontologies/pizza/pizza.owl"
+loadOntology(urionto)
